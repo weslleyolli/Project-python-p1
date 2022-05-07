a34fbd0b701354dcc250848724db02a9f536ef3a
Weslley <weslleyrichardson.cg@hotmail.com>
2022-05-07 15:11:40 -0300

    new def

---
+++ utils.py
@@ -0,0 +1,23 @@
+class List:
+  all_manifestations = ["001#Weslley#Complaints#no water in the drinking fountains", "002#Felipe#Suggestion#add more computers in the class room"]
+  list_suggestion = ["002#Felipe#Suggestion#add more computers in the class room"]
+  list_complaints = ["001#Weslley#Complaints#no water in the drinking fountains"]
+  list_praise = []
+  broken_manifestation = []
+
+class Colors:
+  blue = '\033[34m'
+  red = '\033[31m'
+  normal = '\033[m'
+
+class Functions:
+  options = 0
+  type = 0
+
+def printList(list):
+  for item in list:
+    list_exit = item.replace("#", " - ")
+    print(list_exit)
+def addList(list, protocol):
+  List.all_manifestations.append(protocol)
+  list.append(protocol)
